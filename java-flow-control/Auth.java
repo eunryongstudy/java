65347af9682084e8f2188e51d965ed6abafa9394
eunryong <studyeunryong@gmail.com>
2022-05-06 01:17:56 +0900

    조건문 3

--- java-flow-control/Auth.java
+++ java-flow-control/Auth.java
@@ -6,8 +6,11 @@ public class Auth {
 		String Id = "eunryong";
 		String InputId = args[0];
 		
+		String password = "1111";
+		String Inputpwd = args[1];
+		
 		System.out.println("Hi");
-		if(InputId.equals(Id)) {
+		if(InputId.equals(Id) && Inputpwd.equals(password)) {
 			System.out.println("Master!");
 		} else {
 			System.out.println("Who are you?");
