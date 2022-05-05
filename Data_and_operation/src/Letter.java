15afca175fede363768b92fa3e7a2e39dc12bd59
eunryong <studyeunryong@gmail.com>
2022-05-06 00:13:23 +0900

    Add files via upload

---
+++ Data_and_operation/src/Letter.java
@@ -0,0 +1,11 @@
+public class Letter {
+
+	public static void main(String[] args) {
+		String name = "eunryong";
+		System.out.println("Hello, "+name+" ..."+name+" bye");
+
+		double VAT = 10.0;
+        System.out.println(VAT);
+	}
+
+}
