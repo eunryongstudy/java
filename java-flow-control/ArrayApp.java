ff44fc8854ab933eeeff53b794e3491f2161fd7c
eunryong <studyeunryong@gmail.com>
2022-05-06 01:40:20 +0900

    배열

---
+++ java-flow-control/ArrayApp.java
@@ -0,0 +1,20 @@
+
+public class ArrayApp {
+
+	public static void main(String[] args) {
+		
+		String[] users = new String[3];
+		users[0] = "eunryong";
+        users[1] = "Lee";
+        users[2] = "Kim";
+        
+        System.out.println(users[1]);
+        System.out.println(users.length);
+         
+        int[] scores = {10, 100, 100}; // 원소, element
+        System.out.println(scores[1]);
+        System.out.println(scores.length);
+
+	}
+
+}
