085cfe49f3e096325c6aa0931a4586a7fbef5c47
eunryong <studyeunryong@gmail.com>
2022-05-06 01:00:38 +0900

    bolean

---
+++ java-flow-control/BoleanApp.java
@@ -0,0 +1,16 @@
+
+public class BoleanApp {
+	
+	public static void main(String[] args) {
+		System.out.println("One");
+		System.out.println(1);
+		
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo = "Hello World";
+		
+		System.out.println(foo.contains("World"));
+		System.out.println(foo.contains("eunryong"));
+	}
+}
