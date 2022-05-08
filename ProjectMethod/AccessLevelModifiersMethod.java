a32b35cb5b5d533162fb764f6b4be8c0bd98dbb5
eunryong <studyeunryong@gmail.com>
2022-05-08 20:53:23 +0900

    access level modifiers method

---
+++ ProjectMethod/AccessLevelModifiersMethod.java
@@ -0,0 +1,15 @@
+class Greeting{
+	public static void hi() {
+        System.out.println("Hi");
+    }
+}
+public class AccessLevelModifiersMethod {
+	//public, protected, default, private
+	
+	
+	public static void main(String[] args) {
+		
+		Greeting.hi();
+	}
+
+}
