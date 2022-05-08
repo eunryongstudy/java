59573e5f9ed39f7ea20564cad29005273e5318d3
eunryong <studyeunryong@gmail.com>
2022-05-08 17:48:20 +0900

    method output

--- ProjectMethod/WhyMethod.java
+++ ProjectMethod/WhyMethod.java
@@ -1,20 +1,23 @@
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WhyMethod {
 
-	public static void main(String[] args) {
-		printTwoTimesA("a", "-");
-		printTwoTimesA("a", "*");
-		printTwoTimesA("b", "^");
-		printTwoTimesA("b", "!");
+	public static void main(String[] args) throws IOException {
+		System.out.println(twoTimes("a", "-"));
+		FileWriter fw = new FileWriter("out.txt");
+		fw.write(twoTimes("a", "*"));
+		fw.close();
+//		Email.send("eunryong0405@a.com", "two times a", twoTimes("a", "&"));
 		
 	}
-		
-		public static void printTwoTimesA(String text, String delimiter) {
-	        System.out.println(delimiter);
-	        System.out.println(text);
-	        System.out.println(text);
-		
-
-	}
+		public static String twoTimes(String text, String delimiter) {
+			String out = "";
+			out = out + delimiter +"\n";
+			out = out + text +"\n";
+			out = out + text +"\n";
+			return out;
+			
+		}
 
 }
