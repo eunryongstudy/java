3a1fb6d690c5e6763c6535356d285e6891a5d162
eunryong <studyeunryong@gmail.com>
2022-05-10 18:25:47 +0900

    Instance

--- projectOOP/MyOOP.java
+++ projectOOP/MyOOP.java
@@ -1,32 +1,26 @@
-class print{
-	public static String delimiter = "";
-	public static void A() {
-		System.out.println(delimiter);
-		System.out.println("A");
-		System.out.println("A");
-	}
-	public static void B() {
-		System.out.println(delimiter);
-		System.out.println("B");
-		System.out.println("B");
-	}
-}
 
 public class MyOOP {
 	
 	public static void main(String[] args) {
+		Print p1 = new Print();
+		p1.delimiter = "----";
+		p1.A();
+		p1.A();
+		p1.B();
+		p1.B();
 		
-		print.delimiter = "----";
-		print.A();
-		print.A();
-		print.B();
-		print.B();
+		Print p2 = new Print();
+		p2.delimiter = "****";
+		p2.A();
+		p2.A();
+		p2.B();
+		p2.B();
 		
-		print.delimiter = "****";
-		print.A();
-		print.A();
-		print.B();
-		print.B();
+		p1.A();
+		p2.A();
+		p1.A();
+		p2.A();
+		
 		
 	}
 
